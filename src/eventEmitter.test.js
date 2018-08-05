ff8277dd6770ccdc324ad34ea65694b6aff5faf5
Russell DuBrock <russell.dubrock@gmail.com>
2018-08-04 18:55:50 -0700

    adds some tests

--- src/eventEmitter.test.js
+++ src/eventEmitter.test.js
@@ -1,11 +1,44 @@
 const EventEmitter = require('./eventEmitter.js');
 
 describe('EventEmitter.js', () => {
-  test('emmitter.on should return the unsubscribe function', () => {
-    const emitter = new EventEmitter();
-    const unsubscribeMock = jest.fn();
+  let emitter;
+  const eventHandler = jest.fn();
+  const secondEventHandler = jest.fn();
+  const unsubscribeMock = jest.fn();
+  const buildUnsubscribeMock = jest.fn().mockImplementation(() => unsubscribeMock);
+  beforeEach(() => {
+    emitter = new EventEmitter();
+    eventHandler.mockReset();
+  })
+
+  test('emitter should error with bad arguments', () => {
+    expect(emitter.on).toThrow();
+    expect(emitter.emit).toThrow();
+    expect(emitter.unsubscribe).toThrow();
+    expect(emitter.resetSubscriptionsForEvent).toThrow();
+  });
+
+  test('emitter.on should add subscriptions and return unsubscribe', () => {
+    const unsubscribe = emitter.on('testEvent', () => {});
+    expect(emitter.subscriptions['testEvent'].length).toBe(1);
+    unsubscribe();
+    expect(emitter.subscriptions['testEvent'].length).toBe(0);
+  });
+
+  test('emitter.emit should fire event handlers', () => {
+    emitter.buildUnsubscribe = buildUnsubscribeMock;
     emitter.unsubscribe = unsubscribeMock;
-    const unsubscribe = emitter.on('eventName', () => {});
-    expect(unsubscribe).toBe(unsubscribeMock);
+    emitter.on('myEvent', eventHandler);
+    emitter.on('myEvent', secondEventHandler);
+    emitter.emit('myEvent', 'some', 'arguments', 'could', 'be', 'lots');
+    expect(eventHandler).toHaveBeenCalledWith('some', 'arguments', 'could', 'be', 'lots', unsubscribeMock);
+    expect(secondEventHandler).toHaveBeenCalledWith('some', 'arguments', 'could', 'be', 'lots', unsubscribeMock);
+  });
+
+  test('emitter.once should only fire a subscription once', () => {
+    emitter.once('singleUse', eventHandler);
+    emitter.emit('singleUse', 'withData');
+    emitter.emit('singleUse', 'secondTime');
+    expect(eventHandler).toHaveBeenCalledTimes(1);
   });
 });
